09e98c01b232dfa7bcac87db5ef3208fbc59f0d5
LeinerViloria <lvilora@siesa.com>
2024-03-07 21:49:25 -0500

    Avances

--- MercadoLibre/Models/MenuItem.cs
+++ MercadoLibre/Models/MenuItem.cs
@@ -9,5 +9,7 @@ namespace MercadoLibre.Models
     public class Menu
     {
         public string Name { get; set; } = string.Empty;
+        public bool IsSeparator { get; set; }
+        public bool ShowInfo => !IsSeparator;
     }
 }
