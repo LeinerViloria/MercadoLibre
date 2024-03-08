73af671b692358b6454ce00b56fdeb0eec05cb39
LeinerViloria <lvilora@siesa.com>
2024-03-07 20:21:28 -0500

    Items del navbar

--- MercadoLibre/Pages/NavBar.xaml.cs
+++ MercadoLibre/Pages/NavBar.xaml.cs
@@ -1,9 +1,17 @@
+using System.Collections.ObjectModel;
+using MercadoLibre.Models;
+
 namespace MercadoLibre.Pages;
 
 public partial class NavBar : ContentPage
 {
+	public ObservableCollection<Menu> Items { get; set; }
 	public NavBar()
 	{
 		InitializeComponent();
+
+		Items = new ObservableCollection<Menu>(new () { new Menu() { Name = "Inicio" }, new Menu() { Name = "Inicio 2" } });
+
+		BindingContext = this;
 	}
 }
