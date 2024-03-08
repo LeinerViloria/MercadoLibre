0057e386909b48992e47a8d54325498dfc3d53c9
LeinerViloria <lvilora@siesa.com>
2024-03-08 07:43:14 -0500

    Paginas de navegacion listas

---
+++ MercadoLibre/Pages/MyFavorites.xaml.cs
@@ -0,0 +1,9 @@
+namespace MercadoLibre.Pages;
+
+public partial class MyFavorites : ContentPage
+{
+	public MyFavorites()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MercadoLibre/Pages/Notifications.xaml.cs
@@ -0,0 +1,9 @@
+namespace MercadoLibre.Pages;
+
+public partial class Notifications : ContentPage
+{
+	public Notifications()
+	{
+		InitializeComponent();
+	}
+}
