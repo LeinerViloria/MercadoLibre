4676ed9d93c27bc8a284ab3cd583b01a8a505fa8
LeinerViloria <lvilora@siesa.com>
2024-03-09 11:50:24 -0500

    Se ajustan las vistas

--- MercadoLibre/Pages/MyFavorites.xaml.cs
+++ MercadoLibre/Pages/MyFavorites.xaml.cs
@@ -1,20 +1,11 @@
-using MercadoLibre.Models;
-using System.Collections.ObjectModel;
-
 namespace MercadoLibre.Pages;
 
 public partial class MyFavorites : ContentPage
 {
-	public ObservableCollection<NotificationItem> Items { get; set; } = null!;
 	public MyFavorites()
 	{
 		InitializeComponent();
 
 		BindingContext = this;
-	}
-
-    public void ShowInfo(object Sender, SelectionChangedEventArgs e)
-	{
-
 	}
 }
--- MercadoLibre/Pages/Notifications.xaml.cs
+++ MercadoLibre/Pages/Notifications.xaml.cs
@@ -1,9 +1,20 @@
+using MercadoLibre.Models;
+using System.Collections.ObjectModel;
+
 namespace MercadoLibre.Pages;
 
 public partial class Notifications : ContentPage
 {
+	public ObservableCollection<NotificationItem> Items { get; set; } = null!;
 	public Notifications()
 	{
 		InitializeComponent();
+
+		BindingContext = this;
+	}
+
+	public void ShowInfo(object Sender, SelectionChangedEventArgs e)
+	{
+
 	}
 }
