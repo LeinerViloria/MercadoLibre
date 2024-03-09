d1d7a7f63ef112d15305adf9e15ca6d7afc6f672
LeinerViloria <lvilora@siesa.com>
2024-03-09 18:50:25 -0500

    Avances

--- MercadoLibre/Pages/Home.xaml.cs
+++ MercadoLibre/Pages/Home.xaml.cs
@@ -1,9 +1,23 @@
+using System.Collections.ObjectModel;
+
 namespace MercadoLibre.Pages;
 
 public partial class Home : ContentPage
 {
-	public Home()
+	public ObservableCollection<string> CarouselItems { get; set; } = null!;
+
+    public Home()
 	{
 		InitializeComponent();
+
+		_ = LoadData();
+
+		BindingContext = this;
+	}
+
+	public async Task LoadData()
+	{
+		var Data = await Utils.Utils.ReadJson<List<string>>("CarrouselItems");
+		CarouselItems = new ObservableCollection<string>(Data);
 	}
 }
