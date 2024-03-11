3bb9c9bf3bf7881b1f533ff3c93f558887ccfc9a
LeinerViloria <lvilora@siesa.com>
2024-03-10 22:29:39 -0500

    avances

--- MercadoLibre/Models/ProductItem.cs
+++ MercadoLibre/Models/ProductItem.cs
@@ -6,8 +6,12 @@ namespace MercadoLibre.Models
         public string Image {get; set;} = string.Empty;
         public string Name {get; set;} = string.Empty;
         public int Price {get; set;}
+        public string Value => $"${Price}";
+        public string OfferValue => $"${OfferPrice}";
         public float DiscountPercentage {get; set;}
         public float OfferPrice => Price <= 0 ?
             0 : Price - (Price/100*DiscountPercentage);
+        public bool HasOffer => DiscountPercentage > 0;
+        public bool HasNoOffer => DiscountPercentage <= 0;
     }
 }
