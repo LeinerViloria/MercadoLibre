8ad3d0e885c84c0fb5c7ac78f3de6fc3830e6e84
FaridMendoza22 <faridmendoza9310@hotmail.com>
2024-03-11 23:37:45 -0500

    avances F

--- MercadoLibre/Pages/MyFavorites.xaml.cs
+++ MercadoLibre/Pages/MyFavorites.xaml.cs
@@ -1,11 +1,30 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
 namespace MercadoLibre.Pages;
-
-public partial class MyFavorites : ContentPage
+public partial class MyFavorites : ContentPage, INotifyPropertyChanged
 {
-	public MyFavorites()
-	{
-		InitializeComponent();
+    public ObservableCollection<Favorite> Favorites { get; set; }
 
-		BindingContext = this;
-	}
+    public MyFavorites()
+    {
+        InitializeComponent();
+
+        Favorites = new ObservableCollection<Favorite>();
+
+
+        BindingContext = this;
+    }
+
+    private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        // Filtrar la lista de favoritos
+    }
+}
+
+public class Favorite
+{
+    public string Name { get; set; }
+    public string Price { get; set; }
+    public string Image { get; set; }
 }
