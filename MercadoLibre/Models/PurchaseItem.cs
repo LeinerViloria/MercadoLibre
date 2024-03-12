a915323510c2616ee84e7f741430067c29ec8b71
LeinerViloria <lvilora@siesa.com>
2024-03-11 22:10:30 -0500

    Avances

---
+++ MercadoLibre/Models/PurchaseItem.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MercadoLibre.Models
+{
+    public class PurchaseItem
+    {
+        public string ImageSource { get; set; }
+        public string SellDate { get; set; }
+        public PurchaseState State { get; set; }
+        public string? ArrivedDate { get; set; }
+
+    }
+
+    public enum PurchaseState
+    {
+        Pendiente = 0,
+        Entregado = 1,
+    }
+}
--- MercadoLibre/Pages/MyPurchases.xaml.cs
+++ MercadoLibre/Pages/MyPurchases.xaml.cs
@@ -1,9 +1,54 @@
+using MercadoLibre.Models;
+using Syncfusion.Maui.Core;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
 namespace MercadoLibre.Pages;
 
-public partial class MyPurchases : ContentPage
+public partial class MyPurchases : ContentPage, INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler PropertyChanged;
+    public List<PurchaseItem> Items { get; set; } = null!;
+	public bool IsLoading { get; set; } = true;
+	public bool ShowCollection => !IsLoading;
 	public MyPurchases()
 	{
 		InitializeComponent();
+
+        BindingContext = this;
+
+		_ = LoadData();
 	}
+
+    protected override void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private async Task LoadData()
+	{
+		await SetData();
+
+		BusyIndicator.IsRunning = false;
+
+        IsLoading = false;
+		OnPropertyChanged(nameof(IsLoading));
+		OnPropertyChanged(nameof(ShowCollection));
+    }
+
+	public void LoadMoreCommand(object Sender, EventArgs e)
+	{
+
+	}
+
+	public async Task SetData()
+	{
+		var Data = await Utils.Utils.ReadJson<List<PurchaseItem>>("MyPurchases");
+
+        await Task.Delay(3000);
+
+        Items = Data;
+
+		OnPropertyChanged(nameof(Items));
+    }
 }
